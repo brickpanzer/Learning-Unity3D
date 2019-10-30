2c73324c32fbdbea7a8390fa16b9a3834c647e02
John Gossett <36803194+brickpanzer@users.noreply.github.com>
2019-10-30 15:27:56 -0600

    Dynamic Weapons Pt1
    Weapons are now loaded dynamically from prefabs. Still no way for player to select weapons, and weapons lack audio source

---
+++ Assets/Scripts/PlayerEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEventHandler : MonoBehaviour
+{
+		bool menu_hidden;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu_hidden = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/WeaponHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHandler : MonoBehaviour
+{
+		public GameObject weapon_prefab;
+		GameObject weapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+			weapon = Instantiate(weapon_prefab, transform.position, transform.rotation, this.gameObject.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
